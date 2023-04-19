ea4429b27da9d78c47c8b702d7ccc98b1528c39f
Ray Merrell <RayMerrell@outlook.com>
2023-04-19 14:13:57 +0100

    end

--- src/books/routes.js
+++ src/books/routes.js
@@ -1,14 +1,11 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const bookRouter = Router();
 
-const {addBook}=require("./controllers");
-const {getAllBooks}=require("./controllers");
-const {updateBook}=require("./controllers");
-const {deleteBook}=require("./controllers");
+const { addBook, getAllBooks, updateBook, deleteBook} = require("./controllers");
 
 bookRouter.post("/books/addbook", addBook);
 bookRouter.get("/books/getallbooks", getAllBooks);
-bookRouter.post("/books/updateBook", updateBook);
+bookRouter.put("/books/updateBook", updateBook);
 bookRouter.delete("/books/deleteBook", deleteBook);
 
-module.exports=bookRouter;
+module.exports = bookRouter;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-require ("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT || 5001;
 const Book = require("./books/model");
@@ -7,16 +7,16 @@ const bookRouter = require("./books/routes");
 const app = express();
 app.use(express.json());
 
-const syncTables = ()=>{
-    Book.sync();
-}
+const syncTables = () => {
+  Book.sync();
+};
 app.use(bookRouter);
 
-app.get("/health", (req, res) =>{
-    res.status(200).json({message:"App is healthy"})});
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "App is healthy" });
+});
 
-app.listen(port, ()=>{
-    syncTables();
-    console.log("Server is listening");
-})
-
+app.listen(port, () => {
+  syncTables();
+  console.log("Server is listening");
+});
